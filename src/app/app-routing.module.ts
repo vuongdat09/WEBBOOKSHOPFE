aa4be690eca8dbbe0f4a097927d2ba305d2209c0
envidi <phanm711996@gmail.com>
2023-09-28 11:51:56 +0700

    feature/add-product-detail-UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,15 @@ import { SigninComponent } from './components/auth/signin/signin.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
 import { LayoutWebsiteComponent } from './shared/layout-website/layout-website.component';
 import { HomePageComponent } from './components/page/home-page/home-page.component';
+import { ProductDetailComponent } from './components/page/product-detail/product-detail.component';
+
 
 const routes: Routes = [
   {path: '', component: LayoutWebsiteComponent,children:[
     {path: '', component: HomePageComponent},
     {path: 'signin', component: SigninComponent},
     {path: 'signup', component: SignupComponent},
+    {path: 'product/:id', component: ProductDetailComponent},
   ]},
   {path :'admin', component:NavComponent,children:[
     {path: '', component:DasboarComponent},
